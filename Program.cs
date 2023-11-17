242fb0dc4e87ec0dc5dcaa6bcecd605be633f35c
Sadiqabello <Sadiqabello0@gmail.com>
2023-11-17 17:17:08 +0100

    Refactoring the guided project to accept the score of the student through the terminal

--- Program.cs
+++ Program.cs
@@ -1,34 +1,131 @@
-﻿try
-{
-    Process1();
-}
-catch
-{
-    Console.WriteLine("An exception has occurred");
-}
+﻿using System;
 
-Console.WriteLine("Exit program");
-
-static void Process1()
+class Program
 {
-    try
+    static void Main()
     {
-        WriteMessage();
+        // Guided Project1: GPA Calculator
+        // initialize variables - graded assignments
+        int examAssignments = 5;
+
+        // Student names
+        string[] studentNames = new string[] { "Sophia", "Andrew" };
+        //, "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor"
+
+        int[] studentScores = new int[examAssignments];
+
+        // Declare the variable outside the loop
+        string currentStudentLetterGrade = "";
+
+        // Write the Report Header to the console
+        Console.WriteLine("Student\t\tGrade\n");
+
+        foreach (string name in studentNames)
+        {
+            string currentStudent = name;
+
+            Console.WriteLine($"Enter scores for {currentStudent}:");
+            for (int i = 0; i < examAssignments; i++)
+            {
+                Console.Write($"Enter score for Exam {i + 1}: ");
+                int score;
+                if (int.TryParse(Console.ReadLine(), out score))
+                {
+                    studentScores[i] = score;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                    i--;
+                }
+            }
+
+            // initialize/reset the sum of scored assignments
+            int sumAssignmentScores = 0;
+
+            // initialize/reset the calculated average of exam + extra credit scores
+            decimal currentStudentGrade = 0;
+
+            // initialize/reset a counter for the number of assignment 
+            int gradedAssignments = 0;
+
+            // loop through the scores array and complete calculations for currentStudent
+            foreach (int score in studentScores)
+            {
+                // increment the assignment counter
+                gradedAssignments += 1;
+
+                if (gradedAssignments <= examAssignments)
+                {
+                    sumAssignmentScores += score;
+                }
+                else
+                {
+                    // add the extra credit points to the sum - bonus points equal to 10% of an exam score
+                    sumAssignmentScores += score / 10;
+                }
+            }
+
+            currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+
+            if (currentStudentGrade >= 97)
+            {
+                currentStudentLetterGrade = "A+";
+            }
+            else if (currentStudentGrade >= 93)
+            {
+                currentStudentLetterGrade = "A";
+            }
+            else if (currentStudentGrade >= 90)
+            {
+                currentStudentLetterGrade = "A-";
+            }
+            else if (currentStudentGrade >= 87)
+            {
+                currentStudentLetterGrade = "B+";
+            }
+            else if (currentStudentGrade >= 83)
+            {
+                currentStudentLetterGrade = "B";
+            }
+            else if (currentStudentGrade >= 80)
+            {
+                currentStudentLetterGrade = "B-";
+            }
+            else if (currentStudentGrade >= 77)
+            {
+                currentStudentLetterGrade = "C+";
+            }
+            else if (currentStudentGrade >= 73)
+            {
+                currentStudentLetterGrade = "C";
+            }
+            else if (currentStudentGrade >= 70)
+            {
+                currentStudentLetterGrade = "C-";
+            }
+            else if (currentStudentGrade >= 67)
+            {
+                currentStudentLetterGrade = "D+";
+            }
+            else if (currentStudentGrade >= 63)
+            {
+                currentStudentLetterGrade = "D";
+            }
+            else if (currentStudentGrade >= 60)
+            {
+                currentStudentLetterGrade = "D-";
+            }
+            else
+            {
+                currentStudentLetterGrade = "F";
+            }
+
+            Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+        }
+
+        // required for running in VS Code (keeps the Output windows open to view results)
+        Console.WriteLine("\n\rPress the Enter key to continue");
+        Console.ReadLine();
     }
-    catch
-    {
-        Console.WriteLine("Exception caught in Process1");
-    }
-
-}
-
-static void WriteMessage()
-{
-    double float1 = 3000.0;
-    double float2 = 0.0;
-    int number1 = 3000;
-    int number2 = 0;
-
-    Console.WriteLine(float1 / float2);
-    Console.WriteLine(number1 / number2);
 }
